aa98d1931d2c36d1df9e00d4eedf5eff2c747fa6
33750900053607 <47789111+33750900053607@users.noreply.github.com>
2019-03-17 13:24:59 +0900

    1st commit

---
+++ docs/precache-manifest.05cf577390d4ebc57dcb10fa25577ca3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "b481acc781da09280082",
+    "url": "js/chunk-vendors.2e4ab3f2.js"
+  },
+  {
+    "revision": "76371ca275100d287e92",
+    "url": "js/app.ac091040.js"
+  },
+  {
+    "revision": "26fee0059ec098d5f609",
+    "url": "js/about.d288b4f1.js"
+  },
+  {
+    "revision": "7a1fb49e26bfc6992c213028e3b09509",
+    "url": "index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "76371ca275100d287e92",
+    "url": "css/app.2a57277c.css"
+  }
+];
--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,10 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
     <router-view/>
   </div>
 </template>
